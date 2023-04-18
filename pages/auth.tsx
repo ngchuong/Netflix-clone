b9d2bc43a696277ccd9ad6b4b1d4539c8b246172
chuongng <ngchuong08@gmail.com>
2023-04-19 02:18:14 +0700

    protecting routes, profiles screen

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,5 @@
 import { useCallback, useState } from "react";
 import axios from "axios";
-import { useRouter } from "next/router";
 import Input from "../components/Input";
 import { signIn } from "next-auth/react";
 
@@ -8,7 +7,6 @@ import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
-  const router = useRouter();
 
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
@@ -27,15 +25,12 @@ const Auth = () => {
       await signIn("credentials", {
         email,
         password,
-        redirect: false,
-        callbackUrl: "/",
+        callbackUrl: "/profiles",
       });
-
-      router.push("/");
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
-  }, [email, password, router]);
+  }, [email, password]);
 
   const register = useCallback(async () => {
     try {
@@ -47,7 +42,7 @@ const Auth = () => {
 
       login();
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   }, [email, login, name, password]);
 
@@ -95,13 +90,13 @@ const Auth = () => {
             </button>
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div
-                onClick={() => signIn("google", { callbackUrl: "/" })}
+                onClick={() => signIn("google", { callbackUrl: "/profiles" })}
                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
               >
                 <FcGoogle size={32} />
               </div>
               <div
-                onClick={() => signIn("github", { callbackUrl: "/" })}
+                onClick={() => signIn("github", { callbackUrl: "/profiles" })}
                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
               >
                 <FaGithub size={32} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,36 @@
 import type { NextPage } from "next";
+import { NextPageContext } from "next";
+import { signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
+import useCurrentUser from "../hooks/useCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 const Home: NextPage = () => {
+  const { data: user } = useCurrentUser();
+  
   return (
     <>
       <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+      <p className="text-white">Logged with username: {user?.name}</p>
+      <button className="text-white" onClick={() => signOut()}>
+        Logout
+      </button>
     </>
   );
 };
